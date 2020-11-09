6ae5d9d1a33f3f6132590960009be0c6cb23071b
Cbrito03 <63610450+Cbrito03@users.noreply.github.com>
2020-11-09 12:09:23 -0600

    Version 1.0
    Se agrega conexión remota de MongoDB

--- index.js
+++ index.js
@@ -60,12 +60,29 @@ app.get('/', (req, res) => {
 app.listen(port, app, async () => {
     console.log("[Brito] :: API escuchando en puerto", port, 'Conectando a MongoDb...');
 
-    await mongoose.connect(`mongodb://${process.env.URL_MONGODB}/${process.env.BD_MONGODB}?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false`,
-    					  {useUnifiedTopology: true, useNewUrlParser: true})
+    await mongoose.connect(`mongodb://${process.env.URL_MONGODB}/${process.env.BD_MONGODB}?authSource=admin&replicaSet=replset`,
+    {
+        tls: true,    
+        tlsCAFile: "cert/cert.pem",
+        useUnifiedTopology: true ,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    })
     .then(() => console.log('Conexión establecida a MongoDb'))
     .catch(error => {
         console.log('[Brito] :: No se ha logrado conectar a MongoDb :\n', error)
         console.log('[Brito] :: Deteniendo API...')
         process.exit(1)
     });
+
+    /*await mongoose.connect(`mongodb://${process.env.URL_MONGODB}/${process.env.BD_MONGODB}?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false`,
+    					  {useUnifiedTopology: true, useNewUrlParser: true})
+    .then(() => console.log('Conexión establecida a MongoDb'))
+    .catch(error => {
+        console.log('[Brito] :: No se ha logrado conectar a MongoDb :\n', error)
+        console.log('[Brito] :: Deteniendo API...')
+        process.exit(1)
+    });*/
 });
