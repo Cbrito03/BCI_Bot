793d8abf174cf16c7a0b13158b6c0be8952e77bb
Cbrito03 <63610450+Cbrito03@users.noreply.github.com>
2020-12-14 11:31:07 -0600

    Update bot.js

--- routes/bot.js
+++ routes/bot.js
@@ -402,7 +402,7 @@ router.post('/message', async (req, res) => {
 
 											if(/*axios_CEDU.status*/ true)
 											{
-												if(axios_CEDU.code == 200 || axios_CEDU.code == 409 /*|| axios_CEDU.code == 403*/)
+												if(axios_CEDU.code == 200 || axios_CEDU.code == 409 || axios_CEDU.code == 403)
 												{
 													await local_function.si_autenticado();
 
@@ -438,7 +438,7 @@ router.post('/message', async (req, res) => {
 
 													console.log("Resultado de CEDU :::::::: " + axios_CEDU.code);
 
-													if(axios_CEDU.code == 403 /*406*/ &&  parseInt(num_intentos) <= 1 )
+													if(axios_CEDU.code == 406 &&  parseInt(num_intentos) <= 1 )
 													{                            
 														resultado.action = msj_aut_erroneo.action;
 
